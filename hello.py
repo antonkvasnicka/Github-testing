55b804cf9e367718530b46af75e85c39b78ca023
antonkvasnicka <anton.kvasnicka@student.uts.edu.au>
2025-11-03 15:45:11 +1100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 print("Hello, GitHub! This is my first Python script in a repository.")
+
+numbers = [8, 12, 6, 15, 9]
+print(f"Average: {sum(numbers) / len(numbers)}")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.index("banana")
